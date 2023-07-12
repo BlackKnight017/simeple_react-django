2ab32791c89ec9850261409305338dea003dc0e9
BlackKnight017 <blackknight.oct1@gmail.com>
2023-07-12 01:26:18 -0700

    Created

---
+++ simple_project/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class React(models.Model):
+    employee = models.CharField(max_length=40)
+    department = models.CharField(max_length=200)
+    def __str__(self):
+        return self.employee
